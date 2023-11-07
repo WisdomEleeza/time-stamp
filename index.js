4709180295a10ffcf3dae3f6fad538cab426590c
Wisdom Eleeza <whiy07@gmail.com>
2023-11-07 12:35:27 +0000

    add get request for timestamp(6)

--- index.js
+++ index.js
@@ -39,9 +39,6 @@ app.get("/api/:date?", function (req, res) {
   }
 });
 
-app.get("/api/1451001600000", (req, res) => {
-  return res.json({ unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" })
-})
 
 
 
