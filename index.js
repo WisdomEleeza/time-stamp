281fd8de708786514a81ef243456a6fe6b75f939
Wisdom Eleeza <whiy07@gmail.com>
2023-11-07 11:31:18 +0000

    add get request for timestamp

--- index.js
+++ index.js
@@ -20,8 +20,16 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", function (req, res) {
+  const data = req.params.date
+
+  const timestamp = new Date(data).getDate()
+
+  if(!isNaN(timestamp)) {
+    res.json({ unix: timestamp})
+  } else {
+    res.json({error: "Invalid date"})
+  }
 });
 
 
