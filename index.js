5cffecad785c26ad7bd485bce6a82c9365294519
unknown <whiy07@gmail.com>
2023-11-09 02:18:53 +0000

    Refactor timestamp project(3)

--- index.js
+++ index.js
@@ -18,34 +18,40 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-const isValidData = (data) => data.toUTCString() === "Invalid Date";
+function isValidDate(date) {
+  return !isNaN(date);
+}
 
 // your first API endpoint... 
-app.get("/api/:date?", function (req, res) {
-  let data = new Date(+req.params.date)
+const express = require('express');
+const app = express();
+const port = 3000;
 
-  if(isValidData(date)){
-    date = new Date(req.params.date)
-  // } else {
-  //   res.json({ error: "Invalid Date"})
-  }
+app.get('/api/:date?', function (req, res) {
+  const inputDate = req.params.date;
 
-  if(isValidData(date)){
-    res.json({ error: "Invalid Date" })
-    return
-  }
-
-  res.json({
-    unix: date.getTime(),
-    utc: date.toUTCString()
-  })
-
-  app.get('/api', (req, res) => {
+  if (!inputDate) {
+    // Handle requests with an empty date parameter
+    const currentDate = new Date();
     res.json({
-      unix: new Date().getTime(),
-      utc: new Date().toUTCString()
-    })
-  })
+      unix: currentDate.getTime(),
+      utc: currentDate.toUTCString(),
+    });
+  } else {
+    const date = new Date(inputDate);
+
+    if (isValidDate(date)) {
+      res.json({
+        unix: date.getTime(),
+        utc: date.toUTCString(),
+      });
+    } else {
+      res.json({ error: "Invalid Date" });
+    }
+  }
+});
+
+
 
 
 
